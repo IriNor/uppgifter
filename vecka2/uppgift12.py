9f87e210752d1acc7a3fcd9c7da8d79a6e014831
IriNor <irisa.nord@student.kyh.se>
2021-10-01 15:26:49 +0200

    Alla_uppgift

--- vecka2/uppgift12.py
+++ vecka2/uppgift12.py
@@ -1,12 +1,12 @@
 def is_it_too_long(name):
-return len(name) > 5
+    return len(name) > 5
 def main():
-students = ["anna", "beatrice", "cecilia", "doris", "esmeralda", "frida", "gunilla"]
-for name in students:
-if is_it_too_long(name):
-print(f"{name} är för långt!")
+    students = ["anna", "beatrice", "cecilia", "doris", "esmeralda", "frida", "gunilla"]
+    for name in students:
+        if is_it_too_long(name):
+            print(f"{name} är för långt!")
 if __name__ == '__main__':
-main()
+    main()
 
 # 12.1 Indentera koden så att programmet fungerar hos er!
 # 12.2 Låt användaren skriva listan på studenter som en sträng, t.ex. "kalle,adam,eva" så att listan inte är hårdkodad i programmet
